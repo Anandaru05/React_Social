cb5c611920c0c30a789222fa6abf79b6e0ce1812
Anandakumar A <anandakumar5500@gmail.com>
2023-07-12 16:35:43 +0530

    First Commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h1>About me</h1>
+      <p style={{ marginTop: '2rem' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, amet consequuntur! Aut illum commodi molestiae nulla corporis sapiente. Ab ut ratione incidunt eligendi laborum id repudiandae sequi natus accusamus? Eius!</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/Missing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Missing = () => {
+  return (
+    <main>
+      <h1>Page Not found</h1>
+      <p>Well, thats disappointing..</p>
+      <p>Visit Our Home Page</p>
+    </main>
+  )
+}
+
+export default Missing
